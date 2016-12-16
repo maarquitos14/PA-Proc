4d20f16222de0fc3e0f346393337ba204a3dc996
metbosch <metbosch@outlook.com>
2016-12-16 18:17:31 +0100

    Updated ROB interface to send information to dCache

--- rob.v
+++ rob.v
@@ -11,46 +11,58 @@ module rob(input clk, input rst, input clear,
            /* Input port3 */
            input valid3, input [proc.ROB_IDX_BITS-1:0]robIdx3, input except3, input [proc.ARCH_BITS-1:0]pc3,
            input [proc.ARCH_BITS-1:0]address3, input [proc.ARCH_BITS-1:0]data3, input [proc.REG_IDX_BITS-1:0]dst3, input we3,
-           /* Input port4 */
+           /* Input port4 (Special: it holds writes to dCache) */
            input valid4, input [proc.ROB_IDX_BITS-1:0]robIdx4, input except4, input [proc.ARCH_BITS-1:0]pc4,
            input [proc.ARCH_BITS-1:0]address4, input [proc.ARCH_BITS-1:0]data4, input [proc.REG_IDX_BITS-1:0]dst4, input we4,
+           input weMem4, input wMemByte4,
            /* Output to generate exceptions */
-           output except, output [proc.ARCH_BITS-1:0]address, output [proc.ARCH_BITS-1:0]pc, output [proc.ARCH_BITS-1:0] type,
+           output except, output [proc.ARCH_BITS-1:0]address, output [proc.ARCH_BITS-1:0]pc, output [proc.ARCH_BITS-1:0]type,
            /* Output to register file */
-           output [proc.REG_IDX_BITS-1:0]wDstReg, output [proc.ARCH_BITS-1:0] wData, output wEnable);
+           output [proc.REG_IDX_BITS-1:0]wDstReg, output [proc.ARCH_BITS-1:0]wDataReg, output wEnableReg,
+           /* Interface to dCache */
+           output [proc.ARCH_BITS-1:0]wAddressMem, output [proc.ARCH_BITS-1:0]wDataMem, output wByteMem, output wEnableMem
+);
 
-	parameter TYPE_PORT0 = 1, 
-						TYPE_PORT1 = 2,
-						TYPE_PORT2 = 4,
-						TYPE_PORT3 = 8,
-						TYPE_PORT4 = 16;
+  parameter TYPE_PORT0 = 1, 
+            TYPE_PORT1 = 2,
+            TYPE_PORT2 = 4,
+            TYPE_PORT3 = 8,
+            TYPE_PORT4 = 16;
 
-  reg _validBits[proc.ROB_SLOTS-1:0];
-  reg _exceptBits[proc.ROB_SLOTS-1:0];
-  reg _weBits[proc.ROB_SLOTS-1:0];
-  reg [proc.ARCH_BITS-1:0] _address[proc.ROB_SLOTS-1:0];
-  reg [proc.ARCH_BITS-1:0] _pc[proc.ROB_SLOTS-1:0];
-  reg [proc.ARCH_BITS-1:0] _type[proc.ROB_SLOTS-1:0];
-  reg [proc.ARCH_BITS-1:0] _wData[proc.ROB_SLOTS-1:0];
+  reg                         _validBits[proc.ROB_SLOTS-1:0];
+  reg                         _exceptBits[proc.ROB_SLOTS-1:0];
+  reg                         _weBits[proc.ROB_SLOTS-1:0];
+  reg                         _weMemBits[proc.ROB_SLOTS-1:0];
+  reg                         _wMemByteBits[proc.ROB_SLOTS-1:0];
+  reg [proc.ARCH_BITS-1:0]    _address[proc.ROB_SLOTS-1:0];
+  reg [proc.ARCH_BITS-1:0]    _pc[proc.ROB_SLOTS-1:0];
+  reg [proc.ARCH_BITS-1:0]    _type[proc.ROB_SLOTS-1:0];
+  reg [proc.ARCH_BITS-1:0]    _wDataReg[proc.ROB_SLOTS-1:0];
   reg [proc.REG_IDX_BITS-1:0] _wDstReg[proc.ROB_SLOTS-1:0];
 
-  reg [proc.ROB_IDX_BITS-1:0]_headIdx;
+  reg  [proc.ROB_IDX_BITS-1:0]_headIdx;
   wire [proc.ROB_IDX_BITS-1:0]_headIdxNext;
   wire _validHead;
-	integer i;
+  integer i;
 
-  assign _validHead = _validBits[_headIdx];
-  assign _headIdxNext = _validHead ? ( (_headIdx + 1)%proc.ROB_SLOTS ) : _headIdx;
+  /* Set internal variables */
+    assign _validHead = _validBits[_headIdx];
+    assign _headIdxNext = _validHead ? ( (_headIdx + 1)%proc.ROB_SLOTS ) : _headIdx;
+  /* End set internal variables */
 
   /* Set outputs */
-  assign except = _validHead ? _exceptBits[_headIdx] : 1'b0;
-  assign address = _address[_headIdx];
-  assign type = _type[_headIdx];
-  assign pc = _pc[_headIdx];
-  // NOTE: Next assign may priorize the _validHead information when _weBits is undefined
-  assign wEnable = _validHead && _weBits[_headIdx];
-  assign wData = _wData[_headIdx];
-  assign wDstReg = _wDstReg[_headIdx];
+    assign except = _validHead ? _exceptBits[_headIdx] : 1'b0;
+    assign address = _address[_headIdx];
+    assign type = _type[_headIdx];
+    assign pc = _pc[_headIdx];
+    // NOTE: Next assign must priorize the _validHead information when _weBits is undefined
+    assign wEnableReg = _validHead && _weBits[_headIdx];     
+    assign wDataReg = _wDataReg[_headIdx];
+    assign wDstReg = _wDstReg[_headIdx];
+    assign wEnableMem = _validHead && _weMemBits[_headIdx];
+    assign wDataMem = _wDataReg[_headIdx];
+    assign wAddressMem = _address[_headIdx];
+    assign wByteMem = _wMemByteBits[_headIdx];
   /* End set outputs */
 
   always @(posedge clk) 
@@ -60,9 +72,9 @@ module rob(input clk, input rst, input clear,
       // Clean the memory
       _headIdx <= 4'b0000;
       for( i = 0; i < proc.ROB_SLOTS; i=i+1 ) 
-			begin
-				_validBits[i] = 1'b0;
-			end
+      begin
+        _validBits[i] = 1'b0;
+      end
     end
     else
     begin
@@ -71,7 +83,7 @@ module rob(input clk, input rst, input clear,
     end
   end
 
-	// Handle data from port0
+  // Handle data from port0
   always @(posedge clk) 
   begin
     if(!rst)
@@ -82,16 +94,18 @@ module rob(input clk, input rst, input clear,
         _validBits [robIdx0] <= 1'b1;
         _exceptBits[robIdx0] <= except0;
         _weBits    [robIdx0] <= we0;
+        _weMemBits [robIdx0] <= 1'b0;
+        _wMemByteBits [robIdx0] <= 1'b0;
         _address   [robIdx0] <= address0;
-        _type		   [robIdx0] <= TYPE_PORT0;
+        _type      [robIdx0] <= TYPE_PORT0;
         _pc        [robIdx0] <= pc0;
-        _wData     [robIdx0] <= data0;
+        _wDataReg  [robIdx0] <= data0;
         _wDstReg   [robIdx0] <= dst0;
       end
     end
   end
 
-	// Handle data from port1
+  // Handle data from port1
   always @(posedge clk) 
   begin
     if(!rst)
@@ -102,16 +116,18 @@ module rob(input clk, input rst, input clear,
         _validBits [robIdx1] <= 1'b1;
         _exceptBits[robIdx1] <= except1;
         _weBits    [robIdx1] <= we1;
+        _weMemBits [robIdx1] <= 1'b0;
+        _wMemByteBits [robIdx1] <= 1'b0;
         _address   [robIdx1] <= address1;
-        _type		   [robIdx1] <= TYPE_PORT1;
+        _type      [robIdx1] <= TYPE_PORT1;
         _pc        [robIdx1] <= pc1;
-        _wData     [robIdx1] <= data1;
+        _wDataReg  [robIdx1] <= data1;
         _wDstReg   [robIdx1] <= dst1;
       end
     end
   end
 
-	// Handle data from port2
+  // Handle data from port2
   always @(posedge clk) 
   begin
     if(!rst)
@@ -122,10 +138,12 @@ module rob(input clk, input rst, input clear,
         _validBits [robIdx2] <= 1'b1;
         _exceptBits[robIdx2] <= except2;
         _weBits    [robIdx2] <= we2;
+        _weMemBits [robIdx2] <= 1'b0;
+        _wMemByteBits [robIdx2] <= 1'b0;
         _address   [robIdx2] <= address2;
-        _type		   [robIdx2] <= TYPE_PORT2;
+        _type      [robIdx2] <= TYPE_PORT2;
         _pc        [robIdx2] <= pc2;
-        _wData     [robIdx2] <= data2;
+        _wDataReg  [robIdx2] <= data2;
         _wDstReg   [robIdx2] <= dst2;
       end
     end
@@ -142,10 +160,12 @@ module rob(input clk, input rst, input clear,
         _validBits [robIdx3] <= 1'b1;
         _exceptBits[robIdx3] <= except3;
         _weBits    [robIdx3] <= we3;
+        _weMemBits [robIdx3] <= 1'b0;
+        _wMemByteBits [robIdx3] <= 1'b0;
         _address   [robIdx3] <= address3;
-        _type		   [robIdx3] <= TYPE_PORT3;
+        _type      [robIdx3] <= TYPE_PORT3;
         _pc        [robIdx3] <= pc3;
-        _wData     [robIdx3] <= data3;
+        _wDataReg  [robIdx3] <= data3;
         _wDstReg   [robIdx3] <= dst3;
       end
     end
@@ -162,10 +182,12 @@ module rob(input clk, input rst, input clear,
         _validBits [robIdx4] <= 1'b1;
         _exceptBits[robIdx4] <= except4;
         _weBits    [robIdx4] <= we4;
+        _weMemBits [robIdx0] <= weMem4;
+        _wMemByteBits [robIdx0] <= wMemByte4;
         _address   [robIdx4] <= address4;
-        _type		   [robIdx4] <= TYPE_PORT4;
+        _type      [robIdx4] <= TYPE_PORT4;
         _pc        [robIdx4] <= pc4;
-        _wData     [robIdx4] <= data4;
+        _wDataReg  [robIdx4] <= data4;
         _wDstReg   [robIdx4] <= dst4;
       end
     end
